44d8d0b3790402517a78894ef3f31277171bf271
tabata <masato.tabata@consallink.co.jp>
2023-05-21 02:36:28 +0000

    aa

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,10 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.model.Weather;
 import com.example.demo.service.WeatherService;
 
 @Controller
